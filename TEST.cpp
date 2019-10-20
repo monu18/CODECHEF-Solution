419e7e7b2463682e5854503a5c41cec880b48e8e
Monu Kumar <47026116+monu18@users.noreply.github.com>
2019-10-20 17:13:09 +0530

    Create TEST.cpp

---
+++ TEST.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	while(true)
+	{
+		int temp;
+		cin>>temp;
+		if(temp==42)
+		{
+			break;
+		}
+		else
+		{
+			cout<<temp<<"\n";
+		}
+	}
+	return 0;
+}
